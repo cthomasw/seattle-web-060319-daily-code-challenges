480337ad74017d1066a22549b954c886e4ec2932
Claire Wilbert <claire.wilbert@gmail.com>
2019-06-24 16:38:24 -0700

    Forgot to update my function name to meet requirements initially

--- 01-max-value-in-arry/max_value.rb
+++ 01-max-value-in-arry/max_value.rb
@@ -2,7 +2,7 @@
 
 input_array = [32, 65, 87, 23, 45]
 
-def find_max_value(input_array)
+def maxValue(input_array)
   max_value = 0
   input_array.each do |integer|
     if integer >= max_value
@@ -12,4 +12,4 @@ def find_max_value(input_array)
 	puts max_value
 end
 
-find_max_value(input_array)
+ maxValue(input_array)
